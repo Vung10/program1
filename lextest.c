0d6f9d68ef889e818bb48890efce96bf881d4a64
Vung10 <118209058+Vung10@users.noreply.github.com>
2024-10-01 14:06:39 -0500

    Update lextest.c

--- lextest.c
+++ lextest.c
@@ -28,14 +28,78 @@ main(int argc, char* argv[])
 			break;
 		    // to be completed
 		    case ID:
-			printf("ID);
+			printf("ID");
 			printf(",%d> ", symtable[tokenval].var_index);
-		   
+						break;
 		    case INT8:
 			printf("INT8");
 			printf(",%d> ", tokenval);
 			break;
 		    // to be completed
+		    case INT16:
+			printf("INT36");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case INT32:
+			printf("INT32");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case IF:
+			printf("IF");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case ELSE:
+			printf("ELSE");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case WHILE:
+			printf("WHILE");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case RETURN:
+			printf("RETURN");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case FOR:
+			printf("FOR");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case DO:
+			printf("DO");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case SWITCH:
+			printf("SWITCH");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case CASE:
+			printf("CASE");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case BREAK:
+			printf("BREAK");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case CONTINUE:
+			printf("CONTINUE");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case DEFAULT:
+			printf("DEFAULT");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case FLOAT:
+			printf("FLOAT");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case CHAR:
+			printf("CHAR");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
+		    case INT:
+			printf("INT");
+			printf(",%d> ", symtable[tokenval].var_index);
+			break;
 		}
 	    }
 	}
