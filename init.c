36a9d50fa34a59144251f4821ad808e4206f9748
Vung10 <118209058+Vung10@users.noreply.github.com>
2024-09-30 19:46:02 -0500

    Update init.c

--- init.c
+++ init.c
@@ -1,1 +1,21 @@
+// Name: 
+// Assignment: Program 1 - Part 1
+// Class: CSC453 Fall 2022
+// File: init.c
 
+#include "global.h"
+
+struct entry keywords[] =
+{
+    {"if", IF, -1},
+    // TO BE COMPLETED
+    {0, 0, 0}
+};
+
+void init() {
+    struct entry *p;
+
+    for (p = keywords; p->token; p++) {
+	insert(p->lexptr, p->token, p->var_index);
+    }
+}
