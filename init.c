14e5da0613d54777a25d95850d76d36d1ee11d08
Vung10 <118209058+Vung10@users.noreply.github.com>
2024-09-30 20:07:17 -0500

    Update init.c

--- init.c
+++ init.c
@@ -8,7 +8,19 @@
 struct entry keywords[] =
 {
     {"if", IF, -1},
+    {"else", ELSE, -1},
     // TO BE COMPLETED
+    {"return", RETURN, -1},
+    {"int", INT, -1},
+    {"float", FLOAT, -1},
+    {"char", CHAR, -1},
+    {"for", FOR, -1},
+    {"do", DO, -1},
+    {"switch", SWITCH, -1},
+    {"case", CASE, -1},
+    {"break", BREAK, -1},
+    {"continue", CONTINUE, -1},
+    {"default", DEFAULT, -1},
     {0, 0, 0}
 };
 
